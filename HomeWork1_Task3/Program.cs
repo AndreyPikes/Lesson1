ab089174f72acc071a2b2f3b3d7a5d3168e975b3
Андрей Щукин <1929999@gmail.com>
2021-09-12 23:32:28 +0300

    HomeWork1

---
+++ HomeWork1_Task3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HomeWork1_Task2;
+
+namespace HomeWork1_Task3
+{
+    /*Щукин Андрей
+        ) Написать программу, которая подсчитывает расстояние между точками с координатами x1, y1 и x2,y2 по формуле r = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2).Вывести результат, используя спецификатор формата .2f(с двумя знаками после запятой);
+    б) * Выполнить предыдущее задание, оформив вычисления расстояния между точками в виде метода. */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1 = Input.ReadDouble("Введите X1");
+            double y1 = Input.ReadDouble("Введите Y1");
+            double x2 = Input.ReadDouble("Введите X2");
+            double y2 = Input.ReadDouble("Введите y2");
+
+            Console.WriteLine($"Результат: {Magnitude(x1, y1, x2, y2):#.##}");
+            Console.ReadLine();
+        }
+
+        static double Magnitude(double x1, double y1, double x2, double y2)
+        {
+            return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+        }
+    }
+}
---
+++ HomeWork1_Task5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork1_Task5
+{
+    /*Щукин Андрей
+        а) Написать программу, которая выводит на экран ваше имя, фамилию и город проживания. б) Сделать задание, только вывод организовать в центре экрана.
+в) * Сделать задание б с использованием собственных методов(например, Print(string ms, int x, int y). */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintText("Андрей");
+            PrintText("Щукин");
+            PrintText("Москва");
+
+            Console.ReadLine();
+        }
+
+        static void PrintText(string text)
+        {
+            int positionX = Console.WindowWidth / 2 - text.Length / 2;
+            int positionY = Console.CursorTop == 0 ? Console.WindowHeight / 2 : Console.CursorTop + 1;
+
+            Console.SetCursorPosition(positionX, positionY);
+
+            Console.Write(text);
+        }
+    }
+}
